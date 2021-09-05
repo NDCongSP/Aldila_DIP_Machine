cd214cf76758280cdae8cadccf832587fd87f53d
NDCongSp <ndcong08cddv02@gmail.com>
2021-09-05 08:11:45 +0700

    Main update mainForm

--- AppWinform/AppWinform/MainWindow.xaml.cs
+++ AppWinform/AppWinform/MainWindow.xaml.cs
@@ -23,6 +23,13 @@ namespace AppWinform
         public MainWindow()
         {
             InitializeComponent();
+
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
